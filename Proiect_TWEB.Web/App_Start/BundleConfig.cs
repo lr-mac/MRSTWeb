53631ce55f6f257132ca74e76893b89a9ad98820
Dan Cozaru <dan.cozaru@isa.utm.md>
2023-04-03 17:51:12 +0300

    Updating HTML file, adding BundleConfig

---
+++ Proiect_TWEB.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
+
+namespace Proiect_TWEB.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+        }
+    }
+}
