d7fd1f64b2df60f225966748a5d0eb289ebf0fa8
InsuratelCristi <124879510+InsuratelCristi@users.noreply.github.com>
2023-05-19 16:05:36 +0300

    Added HttpContextExtensions.cs

---
+++ Proiect_TWEB.Helpers/HttpContextExtensions.cs
@@ -0,0 +1,12 @@
+public static class HttpContextExtensions
+{
+	public static UserMinimal GetMySessionObject(this HttpContext current)
+	{
+		return (UserMinimal)current?.Session["__SessionObject"];
+	}
+
+	public static void SetMySessionObject(this HttpContext current, UserMinimal profile)
+	{
+		current.Session.Add("__SessionObject", profile);
+	}
+}
